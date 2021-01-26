9f33879358281aef5ca226424c95d25b920af65d
SUNN\User <sun30595@gmail.com>
2021-01-26 11:55:38 +0700

    Send Assignment Week1

---
+++ GI455_Project/Assets/TextMatching/TextMatching_Script.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextMatching_Script : MonoBehaviour
+{
+
+    public string inputWord;
+    public GameObject inputField, resultDisplay, listDisplay;
+    public string[] wordList;
+
+    private void Start()
+    {
+        for(int i = 0; i < wordList.Length; i++)
+        {
+            listDisplay.GetComponent<Text>().text += (wordList[i] + "\n");
+        }
+    }
+
+    public void SearchInputWord()
+    {
+        inputWord = inputField.GetComponent<Text>().text;
+        
+        for (int i = 0; i < wordList.Length; i++)
+        {
+            if (inputWord == wordList[i])
+            {
+                resultDisplay.GetComponent<Text>().text = "[ " + inputWord + " ]" + " is found.";
+                break;
+            }
+            else
+            {
+                resultDisplay.GetComponent<Text>().text = "[ " + inputWord + " ]" + " is not found.";
+            }
+        }
+    }
+
+}
